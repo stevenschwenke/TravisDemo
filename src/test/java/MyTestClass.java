1aa15ba0bb091a681fc61c4f9fbc1c3a64aac549
Steven Schwenke <steven@stevenschwenke.de>
2018-01-23 06:16:05 +0100

    Initial commit

---
+++ src/test/java/MyTestClass.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MyTestClass {
+
+    @Test
+    void myTest() {
+        assertTrue(Boolean.TRUE);
+    }
+}
